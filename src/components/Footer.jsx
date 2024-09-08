826ac84303f97e4ecbe43e5034f388922408fd3b
mubashar-mizA <mirzamubashar535@gmail.com>
2024-09-08 13:05:58 +0500

    Footer added with github link

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white text-center py-4 m-1">
+            <p>Developed by <a href="https://github.com/mubashar-mizA/" target='_blank'  className="underline text-blue-400">Mubashar Siddique</a></p>
+            <p className="mb-2">© 2024 Explore Countries. All rights reserved.</p>
+            <p className="mt-2">Explore countries, their capitals, regions, and populations to learn more about the world!</p>
+        </footer>
+    );
+}
